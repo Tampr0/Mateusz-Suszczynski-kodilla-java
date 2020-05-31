6223f44ee345b9c6ad0885eb0e8f39d8df594ef7
Mateusz <mateusz@MacBook-Air-Mateusz.local>
2020-06-01 00:23:35 +0200

    Second commit

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,4 +1,16 @@
 package com.kodilla;
 
 public class FirstClass {
+    String title;
+
+    public FirstClass(String title) {
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+
+
+
 }
