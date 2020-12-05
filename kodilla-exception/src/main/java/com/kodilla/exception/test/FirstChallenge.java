10ea1be953d5b45c378038db0be2d8e449422e28
Mateusz <mateusz@MacBook-Air-Mateusz.local>
2020-12-05 01:46:40 +0100

    8.2 task

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+    public double divide(double a, double b) throws ArithmeticException {
+        try {
+        if (b==0) {
+            throw new ArithmeticException();
+        }
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Exception!!" + e);
+        }
+        finally {
+            return a / b;
+        }
+    }
+
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        double result = firstChallenge.divide(3, 0);
+        System.out.println(result);
+    }
+}
