7c6a8911fc74b5b77c9c918fae4a467d6c789800
mateusz <mateusz.suszczynski@gmail.com>
2021-01-28 02:55:10 +0100

    31.2 task

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/config/WebDriverConfig.java
@@ -0,0 +1,25 @@
+package com.kodilla.testing2.google.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class WebDriverConfig {
+
+    public final static String SAFARI = "SAFARI_DRIVER";
+    public final static String CHROME = "CHROME_DRIVER";
+
+    public static WebDriver getDriver(final String driver) {
+        System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/mateusz/Desktop/chromedriver");
+
+        if (driver.equals(SAFARI)) {
+            return new SafariDriver();
+        } else if (driver.equals(CHROME)) {
+            return new ChromeDriver();
+        } else {
+            return null;
+        }
+
+    }
+}
